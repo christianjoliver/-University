90280500428fb11d9097b72157d147ec61aa0017
Christian Oliveira <christianjonasoliver@gmail.com>
2023-03-03 18:53:44 -0300

    Delete Busca Local directory

--- Busca Local/executa.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-reset
-g++ runSuperfBusca.cpp -o superfBuscaExec -lGLU -lGL -lglut
-#g++ /System/Library/Frameworks/GLUT.framework/GLUT /System/Library/Frameworks/OpenGL.framework/OpenGL teste.cpp -o mazeExec
-
-./superfBuscaExec $1
-
-rm superfBuscaExec
